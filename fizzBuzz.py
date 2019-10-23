dcd9b4877d7f695ef1e4c3b8d7b83a0b759ac6cb
FlimothyCrow <discman15@hotmail.com>
2019-10-22 23:14:45 -0600

    added failing test for list containing a string passing into wordChanger

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -5,16 +5,17 @@ def listRange(start, stop):
 
 def wordChanger(numbers, target, replace):
     for (i, item) in enumerate(numbers) :
-        if item % target == 0 :
-            numbers[i] = replace
+            if item % target == 0 :
+                numbers[i] = replace
     return numbers
 
 
 
-def listMutate(numbers, target1, replace1):
+def listMutate(numbers, target1, replace1, target2, replace2):
     mutation = wordChanger(numbers, target1, replace1)
+    wordChanger(mutation, target2, replace2)
 
 
-listMutate([1, 2, 3, 4, 5, 6, 7, 8], 3, "jerk")
+#listMutate([1, 2, 3, 4, 5, 6, 7, 8], 3, "jerk", 2, "schmo")
 
-#def multipleMutate(start, stop) :
+
