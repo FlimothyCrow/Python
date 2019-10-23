14bffab6fecbdc477128f03b602211b1292baf1d
FlimothyCrow <discman15@hotmail.com>
2019-10-22 20:19:43 -0600

    fizzBuzz rebuild

---
+++ fizzBuzz.py
@@ -0,0 +1,7 @@
+def listRange(start, stop):
+    for i in range (start, stop) :
+        print(i, end=", ")
+
+
+
+#listRange(1, 11)
---
+++ fizzBuzztest.py
@@ -0,0 +1,8 @@
+import unittest
+from fizzBuzz import *
+
+
+class PokerTests(unittest.TestCase):
+    def test_listRange(self):
+        self.assertEqual((1,2), listRange(1, 2))
+
