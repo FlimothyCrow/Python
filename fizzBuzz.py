c13b8603bb03067f3ae4dc71aa34d2f8900f5a52
FlimothyCrow <discman15@hotmail.com>
2019-10-22 21:16:21 -0600

    built wordNums

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -2,3 +2,11 @@ def listRange(start, stop):
     return list(range(start, stop))
 
 
+def wordNums(numbers, replace):
+    for (i, item) in enumerate(numbers) :
+        if item % 3 == 0 :
+            numbers[i] = replace
+            return numbers
+
+wordNums([1,2,3,4], "Jerk")
+
--- fizzBuzztest.py
+++ fizzBuzztest.py
@@ -6,3 +6,5 @@ class PokerTests(unittest.TestCase):
     def test_listRange(self):
         self.assertEqual([1,2], listRange(1, 3))
 
+    def test_wordNums(self):
+        self.assertEqual([1, 2, "Jerk", 4], wordNums([1, 2, 3, 4], "Jerk"))
