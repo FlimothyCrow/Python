7173e7ecc8e54a25188dad03b056506743daee2f
FlimothyCrow <discman15@hotmail.com>
2019-10-22 22:38:43 -0600

    added listRange test

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -11,19 +11,10 @@ def wordChanger(numbers, target, replace):
 
 
 
-
-# line 10 is causing a print / return for each occurrence of target
-# return / print is at the end of each loop
-# dedented return outside of loop for correct returns
-
-# this program should take start, stop, target and replacer
-# it will call listRange once and wordChanger twice
-# then print the result
-
 def listMutate(start, stop, target, replace):
     numbers = listRange(start, stop)
     print(wordChanger(numbers, target, replace))
 
-listMutate(1, 21, 3, "fizz")
+#listMutate(1, 21, 3, "fizz")
 
 
--- fizzBuzztest.py
+++ fizzBuzztest.py
@@ -6,3 +6,6 @@ class fizzBuzz(unittest.TestCase):
     def test_wordChanger(self):
         self.assertEqual([1, 2, "Jerk", 4], wordChanger([1, 2, 3, 4], 3, "Jerk"))
         self.assertEqual([1, 2, "Jerk", 4, 5, "Jerk"], wordChanger([1, 2, 3, 4, 5, 6], 3, "Jerk"))
+
+    def test_listRange(self):
+        self.assertEqual([1, 2, 3, 4, 5], listRange(1, 6))
