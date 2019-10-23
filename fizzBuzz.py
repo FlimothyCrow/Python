96bcde519903cf29603ee00b43381bc7a305d067
FlimothyCrow <discman15@hotmail.com>
2019-10-22 21:33:45 -0600

    created listMutate

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -2,11 +2,20 @@ def listRange(start, stop):
     return list(range(start, stop))
 
 
-def wordNums(numbers, replace):
+def wordChanger(numbers, target, replace):
     for (i, item) in enumerate(numbers) :
-        if item % 3 == 0 :
+        if item % target == 0 :
             numbers[i] = replace
             return numbers
 
-wordNums([1,2,3,4], "Jerk")
+# this program should take start, stop, and two replacers
+# it will call listRange once and wordChanger twice
+# then print the result
+
+def listMutate(start, stop, target, replace):
+    numbers = listRange(start, stop)
+    print(wordChanger(numbers, target, replace))
+
+listMutate(1, 4, 3, "schmo")
+
 
