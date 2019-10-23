c0840535b396421f25c7a28337b6ea8fdd79b617
FlimothyCrow <discman15@hotmail.com>
2019-10-22 21:41:10 -0600

    added test to wordChanger for multiple replacement occurrences

--- fizzBuzztest.py
+++ fizzBuzztest.py
@@ -8,3 +8,4 @@ class PokerTests(unittest.TestCase):
 
     def test_wordNums(self):
         self.assertEqual([1, 2, "Jerk", 4], wordChanger([1, 2, 3, 4], 3, "Jerk"))
+        self.assertEqual(["schmo", 4, 5, "schmo"], wordChanger([3, 4, 5, 6], 3, "schmo"))
