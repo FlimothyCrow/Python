c3010cc83f9a0c7ce66ba0717eb7c6dc876787c7
FlimothyCrow <discman15@hotmail.com>
2019-10-22 22:34:18 -0600

    dedented return at the end of wordChanger to return final product, not iterations

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -7,13 +7,14 @@ def wordChanger(numbers, target, replace):
     for (i, item) in enumerate(numbers) :
         if item % target == 0 :
             numbers[i] = replace
-            return numbers
+    return numbers
 
 
 
 
 # line 10 is causing a print / return for each occurrence of target
-
+# return / print is at the end of each loop
+# dedented return outside of loop for correct returns
 
 # this program should take start, stop, target and replacer
 # it will call listRange once and wordChanger twice
@@ -23,12 +24,6 @@ def listMutate(start, stop, target, replace):
     numbers = listRange(start, stop)
     print(wordChanger(numbers, target, replace))
 
-#listMutate(1, 21, 3, "fizz")
+listMutate(1, 21, 3, "fizz")
 
-
-mylist = [111, -222, 333, -444, -1, -2, -3]
-for (i, item) in enumerate(mylist):
-    if item < 0:
-        mylist[i] = 0
-#print(mylist)
 
--- fizzBuzztest.py
+++ fizzBuzztest.py
@@ -3,6 +3,6 @@ from fizzBuzz import *
 
 
 class fizzBuzz(unittest.TestCase):
-
     def test_wordChanger(self):
         self.assertEqual([1, 2, "Jerk", 4], wordChanger([1, 2, 3, 4], 3, "Jerk"))
+        self.assertEqual([1, 2, "Jerk", 4, 5, "Jerk"], wordChanger([1, 2, 3, 4, 5, 6], 3, "Jerk"))
