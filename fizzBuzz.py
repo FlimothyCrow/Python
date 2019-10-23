9e8f07c62f9a2c931554ad4c50c731d41a356b5d
FlimothyCrow <discman15@hotmail.com>
2019-10-22 22:22:59 -0600

    added test to wordChanger for multiple replacement occurrences

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,3 +1,4 @@
+
 def listRange(start, stop):
     return list(range(start, stop))
 
@@ -7,6 +8,12 @@ def wordChanger(numbers, target, replace):
         if item % target == 0 :
             numbers[i] = replace
             return numbers
+
+
+
+
+# line 10 is causing a print / return for each occurrence of target
+
 
 # this program should take start, stop, target and replacer
 # it will call listRange once and wordChanger twice
@@ -18,4 +25,10 @@ def listMutate(start, stop, target, replace):
 
 #listMutate(1, 21, 3, "fizz")
 
+
+mylist = [111, -222, 333, -444, -1, -2, -3]
+for (i, item) in enumerate(mylist):
+    if item < 0:
+        mylist[i] = 0
+#print(mylist)
 
--- fizzBuzztest.py
+++ fizzBuzztest.py
@@ -2,10 +2,7 @@ import unittest
 from fizzBuzz import *
 
 
-class PokerTests(unittest.TestCase):
-    def test_listRange(self):
-        self.assertEqual([1,2], listRange(1, 3))
+class fizzBuzz(unittest.TestCase):
 
-    def test_wordNums(self):
+    def test_wordChanger(self):
         self.assertEqual([1, 2, "Jerk", 4], wordChanger([1, 2, 3, 4], 3, "Jerk"))
-        self.assertEqual(["schmo", 4, 5, "schmo"], wordChanger([3, 4, 5, 6], 3, "schmo"))
