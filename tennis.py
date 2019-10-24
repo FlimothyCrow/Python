e56240d57b46ff3e2859db185c1306e8956431a6
FlimothyCrow <discman15@hotmail.com>
2019-10-24 15:56:35 -0600

    updated test case for tennis

--- tennis.py
+++ tennis.py
@@ -9,6 +9,5 @@ def score(num) :
         return "forty"
 
 def tennis(p1, p2) :
-    if p1 == p2 :
-        return "fifteen love"
+    return score(p1) + " " + score(p2)
 
--- tennistest.py
+++ tennistest.py
@@ -5,7 +5,7 @@ from tennis import *
 class tennistest(unittest.TestCase):
 
     def test_tennis(self):
-        self.assertEqual("fifteen love", tennis(1,1))
+        self.assertEqual("fifteen love", tennis(1,0))
         self.assertEqual("fifteen thirty", tennis(1,2))
 
     def test_score(self):
