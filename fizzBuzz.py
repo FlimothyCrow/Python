fd148bf60eb70be48b40ddd07e35f28b71ac7cf7
FlimothyCrow <discman15@hotmail.com>
2019-10-23 20:05:16 -0600

    rebuilt conditional for num == int failure

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,4 +1,3 @@
-
 def listRange(start, stop):
     return list(range(start, stop))
 
@@ -6,17 +5,24 @@ def listRange(start, stop):
 def wordChanger(numbers, target, replace):
     newList = []
     for num in numbers :
-        if num % target == 0 :
-            newList.append(replace)
+        if isinstance(num, int) :
+            if num % target == 0 :
+                newList.append(replace)
+            else :
+                newList.append(num)
         else :
             newList.append(num)
     return newList
 
 
+
 def listMutate(numbers, target1, replace1, target2, replace2):
     mutation = wordChanger(numbers, target1, replace1)
-    wordChanger(mutation, target2, replace2)
+    return wordChanger(mutation, target2, replace2)
 
 
-listMutate([1, 2, 3, 4, 5, 6, 7, 8], 3, "jerk", 2, "schmo")
+
+#print(listMutate([1, 2, 3, 4, 5, 6, 7, 8], 3, "jerk", 2, "schmo"))
+
+
 
