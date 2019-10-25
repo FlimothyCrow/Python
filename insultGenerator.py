7d17d144ed5723201fd2a4bb7f617103a1f5776c
FlimothyCrow <discman15@hotmail.com>
2019-10-25 10:44:12 -0600

    added parseInsult

--- insultGenerator.py
+++ insultGenerator.py
@@ -12,6 +12,7 @@ shakespeare = ["goatish", "milk-livered", "poorly born", "tardy-gaited", "yeasty
 # we need to refer to each list and pull the element into a string (with enumerate maybe?)
 # insultGenerator will take a string name and pull an element at random
 # could we set it to choose from the lists in a random order?
+# parsing data into a data structure is a huge part of the job
 
 def insultGenerator(set0, set1, set2) :
     finalInsult = (random.choice(set0))
@@ -19,4 +20,20 @@ def insultGenerator(set0, set1, set2) :
     finalInsult = finalInsult + " " + (random.choice(set2))
     print(finalInsult)
 
-insultGenerator(modern, shakespeare, nouns)
+#insultGenerator(modern, shakespeare, nouns)
+
+# split by string with newline
+# split by comma
+# assign each line to a variable
+# contents gives us one long string
+f = open("insults.txt", "r")
+contents = f.read()
+#print(contents)
+
+def parseInsult(row, inputText):
+    final = []
+    rows = inputText.splitlines(row)
+    singleRow = rows[0].split(",")
+    for i in singleRow :
+        final.append(i.strip())
+    return final
--- insultGeneratorTest.py
+++ insultGeneratorTest.py
@@ -6,3 +6,8 @@ class insultGeneratorTest(unittest.TestCase):
 
     def test_randomNumber(self):
         self.assertEqual(1, randomNumber())
+
+    def test_parseInsult(self):
+        self.assertEqual(["a", "b"], parseInsult(0, "a, b\nc, d\n, e, f"))
+        self.assertEqual(["c", "d"], parseInsult(1, "a, b\nc, d\n, e, f"))
+        self.assertEqual(["e", "f"], parseInsult(2, "a, b\nc, d\n, e, f"))
