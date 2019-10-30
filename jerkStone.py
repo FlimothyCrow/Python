bdef7656d83cb6e7169896ed44e58824fdf5db6b
FlimothyCrow <discman15@hotmail.com>
2019-10-30 06:25:52 -0600

    updated startDeck

--- jerkStone.py
+++ jerkStone.py
@@ -3,12 +3,19 @@ import random
 gameState = {'health': 30,
              'mana': 10,
              'hand':[1, 2, 3],
-             'deck':[3, 5, 7, 4, 3, 5, 8, 9, 9, 10, 10, 1, 2, 1]}
+             }
+
 
 def cardGenerator() :
     return random.randint(1, 11)
 
-#def startDeck(state) :
+def startDeck(state) :
+    drawnCards = []
+    state["deck"] = drawnCards
+    for i in range(21) :
+        drawnCards.append(cardGenerator())
+    return state
+
 
 def drawCard(state) :
     deck = state.get('deck')
