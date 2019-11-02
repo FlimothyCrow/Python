64a587ec08a5863536b237bd46586d7d4322cd6c
FlimothyCrow <discman15@hotmail.com>
2019-11-02 13:36:47 -0600

    added test cases

--- mergeSort.py
+++ mergeSort.py
@@ -2,6 +2,8 @@ def sortNumerical(numbers) :
     inOrder = sorted(numbers)
     return inOrder
 
+# note that [0, 0, 0, 1] is "smaller" than [0, 0, 1]
+
 def sortReverse(digits) :
     reverseOrder = sorted(digits, reverse=True)
     return reverseOrder
--- mergeSortTest.py
+++ mergeSortTest.py
@@ -8,6 +8,8 @@ class mergeSortTest(unittest.TestCase):
         self.assertEqual([1, 2, 3, 4, 5], sortNumerical([5, 3, 4, 1, 2]))
         self.assertEqual([[1, 2], [1,3], [4, 0]], sortNumerical([[4,0], [1,2], [1,3]]))
 
+
     def test_sortReverse(self):
         self.assertEqual([5, 4, 3, 2, 1], sortReverse([3, 4, 1, 5, 2]))
         self.assertEqual([[3,0], [1,0], [0,3], [0,1]], sortReverse([[1,0], [3,0], [0,3], [0,1]]))
+        self.assertEqual([[0, 1], [0, 0, 1], [0, 0, 0, 1]], sortReverse([[0, 1], [0, 0, 1], [0, 0, 0, 1]]))
