02651456387009a869b8a420ea3a55ed5cecbc44
FlimothyCrow <discman15@hotmail.com>
2019-11-02 13:27:58 -0600

    defined sortReverse

--- mergeSort.py
+++ mergeSort.py
@@ -1,3 +1,8 @@
 def sortNumerical(numbers) :
     inOrder = sorted(numbers)
     return inOrder
+
+def sortReverse(digits) :
+    reverseOrder = sorted(digits, reverse=True)
+    return reverseOrder
+
