2bc738d55359e8815cb4c8b086538433a9ff3eff
FlimothyCrow <discman15@hotmail.com>
2019-11-02 13:30:42 -0600

    added test case to sortNumerical for list of lists

--- mergeSortTest.py
+++ mergeSortTest.py
@@ -6,6 +6,7 @@ class mergeSortTest(unittest.TestCase):
 
     def test_wordChanger(self):
         self.assertEqual([1, 2, 3, 4, 5], sortNumerical([5, 3, 4, 1, 2]))
+        self.assertEqual([[1, 2], [1,3], [4, 0]], sortNumerical([[4,0], [1,2], [1,3]]))
 
     def test_sortReverse(self):
         self.assertEqual([5, 4, 3, 2, 1], sortReverse([3, 4, 1, 5, 2]))
