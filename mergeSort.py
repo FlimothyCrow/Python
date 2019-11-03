9b8ed66a169d6a1a9379d2b03b8fb4a6fdc7ba3f
FlimothyCrow <discman15@hotmail.com>
2019-11-03 11:17:01 -0700

    refactored listGet

--- mergeSort.py
+++ mergeSort.py
@@ -15,9 +15,6 @@ def sortReverse(digits) :
 spicyList = [1, 31, 8, 29, 100, 5, 2, 9, 50]
 dumbList = []
 
-def listExist(numbers) :
-    if len(numbers) > 0 :
-        return True
 
 def mergeSort(numbers) :
     sortedList = []
@@ -29,11 +26,12 @@ def mergeSort(numbers) :
         sortedLeft = mergeSort(left)
         sortedRight = mergeSort(right)
 
-        while listExist(sortedLeft) or listExist(sortedRight) :
-            if listExist(sortedLeft) and not listExist(sortedRight) :
+        while len(sortedLeft) > 0 or len(sortedRight) > 0: # if either has mass
+
+            if len(sortedLeft) > 0 and not len(sortedRight) > 0: # if left has mass
                 sortedList.append(sortedLeft[0])
                 sortedLeft.pop(0)
-            elif listExist(sortedRight) and not listExist(sortedLeft):
+            elif len(sortedRight) > 0 and not len(sortedLeft) > 0: # if right has mass
                 sortedList.append(sortedRight[0])
                 sortedRight.pop(0)
             elif sortedLeft[0] < sortedRight[0] : # both have mass
