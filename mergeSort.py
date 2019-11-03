9db599371ca61726fc34ad0bc5d31c3916d90899
FlimothyCrow <discman15@hotmail.com>
2019-11-03 15:43:36 -0700

    comment trim

--- mergeSort.py
+++ mergeSort.py
@@ -1,17 +1,3 @@
-def sortNumerical(numbers) :
-    inOrder = sorted(numbers)
-    return inOrder
-
-# note that [0, 0, 0, 1] is "smaller" than [0, 0, 1]
-
-def sortReverse(digits) :
-    reverseOrder = sorted(digits, reverse=True)
-    return reverseOrder
-
-# [1, "a", 9, "x", 3, "b"], we've confirmed that sorted can't work with ints and letters together
-# ["alphabet", "jerk store", "every time I die", "Curly's a dope"]
-# capitals ARE taken into account, and are considered "smaller", they will list first
-
 spicyList = [1, 31, 8, 29, 100, 5, 2, 9, 50]
 dumbList = []
 
@@ -43,5 +29,3 @@ def mergeSort(numbers) :
         return sortedList
     else :
         return numbers
-
-print(mergeSort(spicyList))
