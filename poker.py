cd6ddb0b845dbfe7ef82820a3d0ba6c3dcbf8274
FlimothyCrow <discman15@hotmail.com>
2019-11-06 15:22:13 -0700

    passed first test with makeCard

--- poker.py
+++ poker.py
@@ -1,81 +1,26 @@
 
-def cardValueList(hand) :
-    return list(map(cardValue, hand.split()))
+'''
+hand -> card[0] -> value
+                -> suit
+     -> card[1] -> value
+                -> suit
+'''
 
-def checkFrequency(cardvalues) :
-    counts = {}
-    for cardvalue in cardvalues:
-        if cardvalue in counts:
-            counts[cardvalue] = counts[cardvalue] + 1
-        else:
-            counts[cardvalue] = 1
-    return counts
+# DONT EVER PUT LOGIC IN THE CONSTRUCTOR
 
-def highCard(hand) :
-    cards = hand.split()
-    currentHigh = cards[0]
-    for card in cards :
-        if cardValue(card) > cardValue(currentHigh) :
-            currentHigh = card
-    return currentHigh
-
-def cardValue(card) :
-    if card[0] == "K" :
-        return 13
-    if card[0] == "J" :
-        return 11
-    if card[0] == "Q" :
-        return 12
-    if card[0] == "A" :
-        return 14
-    if card[1] == "0" :
-        return 10
-    else :
-        return int(card[0])
-
-# FOR DOESN'T WORK ON DICTIONARIES
-# ITEMS CONVERTS INTO A LIST (PROB OF TUPLES)
-def pairCheck(hand) :
-    values = cardValueList(hand)
-    freqDict = checkFrequency(values)
-    for card,count in freqDict.items() :
-        if (2 in (dict.values(freqDict))) and (3 in (dict.values(freqDict))) :
-            return "full house", max(freqDict.keys()), min(freqDict.keys())
-        if count == 2 :
-            return "pair of {}" .format(card)
-        if count == 3 :
-            return "three of a kind {}" .format(card)
-        if count == 4 :
-            return "four of a kind {}" .format(card)
-
-def orderCheck(hand) :
-    newList = sorted(cardValueList(hand))
-    if newList[4] - newList[0] == 4 :
-        return "straight {} through {}" .format(newList[0], newList[4])
-
-def getSuit(card) :
-    return card[-1]
-
-def suitList(hand) :
-    suits = checkFrequency(map(getSuit, hand.split()))
-    for suit,count in suits.items():
-        if count == 5:
-            return suit
-
-def pokerHands(hand) :
-    if suitList(hand) : #straight flush
-        if orderCheck(hand) :
-            return orderCheck(hand),suitList(hand)
-    if suitList(hand) : #flush
-        return suitList(hand)
-    if pairCheck(hand) : #2, 3 or 4 of a kind, full house
-        return pairCheck(hand)
-    if orderCheck(hand): #straight
-        return orderCheck(hand)
-    else :
-        return "high card", highCard(hand)
+# NO NOT EVEN THAT KIND
 
 
-    
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
 
+class Hand:
+    def __init__(self, Cards):
+        self.cards = Cards
+
+def makeCard(cardString) :
+    cardValue = int(cardString[:-1])
+    return Card(cardString[-1], cardValue)
 
--- pokertests.py
+++ pokertests.py
@@ -3,49 +3,16 @@ from poker import *
 
 
 class PokerTests(unittest.TestCase):
-    def test_cardValue(self):
-        self.assertEqual(13, cardValue("KD"))
-        self.assertEqual(11, cardValue("JS"))
-        self.assertEqual(12, cardValue("QS"))
-        self.assertEqual(14, cardValue("AC"))
-        self.assertEqual(5, cardValue("5H"))
-        self.assertEqual(10, cardValue("10H"))
 
-    def test_highCard(self):
-        self.assertEqual("4D", highCard("3H 2D 4D"))
-        self.assertEqual("KD", highCard("3H KD 2D 4D"))
-        self.assertEqual("5S", highCard("3H 5S 2D 4D"))
-        self.assertEqual("10C", highCard("3S 9H 10C 7S 6D"))
+    def test_makeCard(self):
+        card = makeCard("6S")
+        self.assertEqual(6, card.value)
+        self.assertEqual("S", card.suit)
 
-    def test_pairCheck(self):
-        self.assertEqual(('full house', 13, 3), pairCheck("KS KD KC 3H 3C"))
-        self.assertEqual("pair of 4", pairCheck("4H 9S KC 4D AS"))
-        self.assertEqual(None, pairCheck("3H 9S KC 4D AS"))
-        self.assertEqual("three of a kind 8", pairCheck("8H 8S 5C 8D AS"))
-        self.assertEqual("four of a kind 13", pairCheck("KS KD KC KH 4C"))
+'''
 
-    def test_checkFrequency(self):
-        self.assertEqual({4:3, 9:1}, checkFrequency([4,9,4, 4]))
-
-    def test_orderCheck(self):
-        self.assertEqual("straight 2 through 6", orderCheck("2D 3S 4C 5S 6H"))
-        self.assertEqual("straight 10 through 14", orderCheck("JS AD KC QH 10S"))
-        self.assertEqual(None, orderCheck("JS AD KC QH 9S"))
-
-    def test_cardValueList(self):
-        self.assertEqual([2, 3, 4, 5, 6], cardValueList("2D 3S 4C 5S 6H"))
-
-    def test_suitList(self):
-        self.assertEqual("H", suitList("8H AH 3H JH KH"))
-        self.assertEqual(None, suitList("8H AH 3S JH KH"))
-
-    def test_pokerHands(self):
-        self.assertEqual("straight 3 through 7", pokerHands("3D 4C 5S 6H 7C"))
-        self.assertEqual("pair of 10", pokerHands("10D 10C 8S KC AH"))
-        self.assertEqual("H", pokerHands("3H AH QH 7H 6H"))
-        self.assertEqual("S", pokerHands("3S AS QS 7S 6S"))
-        self.assertEqual("three of a kind 8", pokerHands("8S 8C 8D 2D 3S"))
-        self.assertEqual("four of a kind 9", pokerHands("9S 9C 9D 2D 9S"))
-        self.assertEqual(('straight 3 through 7', 'H'), pokerHands("3H 4H 5H 7H 6H"))
-        self.assertEqual(('full house', 9, 5), pairCheck("9S 9C 5D 5H 9D"))
-        self.assertEqual(('high card', '10D'), pokerHands("10D 2C 8S 3C 5H"))
+    def test_makeHand(self):
+        hand = makeHand("KS KS KS KS KS")
+        self.assertEqual(13, hand.cards[0].value)
+        self.assertEqual("S", hand.cards[0].suit)
+'''
