81e8135c5a1b03600ecf9ca6ef5e979b0cc5c05a
FlimothyCrow <discman15@hotmail.com>
2019-11-25 16:43:51 -0700

    defined class Hand, makeHand function and passing tests

--- poker.py
+++ poker.py
@@ -3,7 +3,11 @@ class Card :
         self.value = value
         self.suit = suit
 
-def makeCard(string) :
+class Hand :
+    def __init__(self, Cards):
+        self.cards = Cards
+
+def makeCard(string) : # takes a string of value and suit "3C" and makes it into a Card class
     if string[0] == "A" :
         cardValue = 14
     elif string[0] == "K":
@@ -16,3 +20,9 @@ def makeCard(string) :
         cardValue = int(string[:-1])
     return Card(cardValue, string[-1])
 
+def makeHand(cards) : # turns a string of "AS KS QC" into a Hand class
+    cardsList = []
+    cards = cards.split() # otherwise we'd get " " as it counts every character
+    for card in cards :
+        cardsList.append(makeCard(card))
+    return Hand(cardsList)
