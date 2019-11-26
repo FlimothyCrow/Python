e436b1ac8fb7a0ae0bcd27cb351a9378814e5940
FlimothyCrow <discman15@hotmail.com>
2019-11-26 14:28:44 -0700

    defined Hand class and makeCard with passing tests

--- poker.py
+++ poker.py
@@ -3,26 +3,16 @@ class Card :
         self.value = value
         self.suit = suit
 
-class Hand :
-    def __init__(self, Cards):
-        self.cards = Cards
 
-def makeCard(string) : # takes a string of value and suit "3C" and makes it into a Card class
+def makeCard(string) :
     if string[0] == "A" :
         cardValue = 14
-    elif string[0] == "K":
+    elif string[0] == "K" :
         cardValue = 13
-    elif string[0] == "Q":
+    elif string[0] == "Q" :
         cardValue = 12
-    elif string[0] == "J":
+    elif string[0] == "J" :
         cardValue = 11
     else :
         cardValue = int(string[:-1])
     return Card(cardValue, string[-1])
-
-def makeHand(cards) : # turns a string of "AS KS QC" into a Hand class
-    cardsList = []
-    cards = cards.split() # otherwise we'd get " " as it counts every character
-    for card in cards :
-        cardsList.append(makeCard(card))
-    return Hand(cardsList)
--- pokertests.py
+++ pokertests.py
@@ -7,15 +7,3 @@ class PokerTests(unittest.TestCase):
     def test_makeCard(self):
         card = makeCard("6S")
         self.assertEqual(6, card.value)
-
-        card0 = makeCard("10C")
-        self.assertEqual(10, card0.value)
-
-        card1 = makeCard("QH")
-        self.assertEqual(12, card1.value)
-
-    def test_makeHand(self):
-        hand = makeHand("AS KS KS JC 6S")
-        self.assertEqual(14, hand.cards[0].value)
-        self.assertEqual(6, hand.cards[4].value)
-        self.assertEqual(11, hand.cards[3].value)
