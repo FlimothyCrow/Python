dcd0fed68cc6b64c2296bf66cb2c284495c24430
FlimothyCrow <discman15@hotmail.com>
2019-11-29 18:19:51 -0700

    defined class Card, makeCard and tests

--- poker.py
+++ poker.py
@@ -3,99 +3,15 @@ class Card :
         self.value = value
         self.suit = suit
 
-
-class Hand :
-    def __init__(self, cards):
-        self.cards = cards
-
-
 def makeCard(string) :
-    if string[0] == "A" :
-        cardValue = 14
-    elif string[0] == "K" :
+    if string[:-1] == "K" :
         cardValue = 13
-    elif string[0] == "Q" :
+    elif string[:-1] == "A" :
+        cardValue = 14
+    elif string[:-1] == "Q":
         cardValue = 12
-    elif string[0] == "J" :
+    elif string[:-1] == "J":
         cardValue = 11
     else :
         cardValue = int(string[:-1])
     return Card(cardValue, string[-1])
-
-
-def makeHand(stringOfCards) :
-    cards = []
-    for card in stringOfCards.split(" ") :
-        cards.append(makeCard(card))
-    return Hand(cards)
-
-
-def cardValues(hand) :
-    values = []
-    for card in hand.cards :
-        values.append(card.value)
-    return values
-
-
-def cardCounter(hand) :
-    cardsDict = {}
-    for value in cardValues(hand) :
-        if value not in cardsDict.keys() :
-            cardsDict.update({value : 1})
-        else :
-            cardsDict[value] += 1
-    return cardsDict
-
-
-def matchReturn(hand) :
-    values = cardCounter(hand).values()
-    for card in values :
-        if 2 in values and 3 in values :
-            return "full house"
-        if card == 2 :
-            return "pair"
-        elif card == 3 :
-            return "three"
-        elif card == 4 :
-            return "four"
-
-
-def suitCounter(hand) :
-    suitsDict = {}
-    for card in hand.cards :
-        if card.suit not in suitsDict :
-            suitsDict.update({card.suit : 1})
-        else :
-            suitsDict[card.suit] += 1
-    return suitsDict
-
-
-def isStraight(hand) :
-    smallest = int(min(cardValues(hand)))
-    largest = int(max(cardValues(hand)))
-    if largest - smallest == 4 :
-        return "Straight {} through {}".format(smallest, largest)
-
-
-def highCard(hand) :
-    values = cardValues(hand)
-    return max(values)
-
-
-def controller(hand) :
-    if 5 in suitCounter(hand).values() :
-        return "flush"
-    elif matchReturn(hand) :
-        return matchReturn(hand)
-    elif isStraight(hand) :
-        return isStraight(hand)
-    else :
-        return highCard(hand)
-
-
-def printer() :
-    cards = input("Choose five cards")
-    hand = makeHand(cards)
-    print(controller(hand))
-
-printer()
--- pokertests.py
+++ pokertests.py
@@ -5,122 +5,13 @@ from poker import *
 class PokerTests(unittest.TestCase):
 
     def test_makeCard(self):
-        card = makeCard("6S")
-        self.assertEqual(6, card.value)
-        ###########
-        card = makeCard("KS")
+        card = makeCard("KD")
         self.assertEqual(13, card.value)
 
-        card = makeCard("QD")
-        self.assertEqual(12, card.value)
-
-        card = makeCard("AC")
+    def test_makeCard1(self):
+        card = makeCard("AD")
         self.assertEqual(14, card.value)
 
-        card = makeCard("JS")
-        self.assertEqual(11, card.value)
-        self.assertEqual("S", card.suit)
-
-    ###########
-    def test_makeHand(self):
-        hand = makeHand("KS 3D 5H 9H KD")
-        self.assertEqual(13, hand.cards[0].value)
-        self.assertEqual("S", hand.cards[0].suit)
-        self.assertEqual(9, hand.cards[3].value)
-
-    ###########
-    def test_cardValues(self):
-# arrange NO LOGIC, PRIMITIVES ONLY OR CONSTRUCTORS (THEY HAVE NO LOGIC)
-        hand = Hand([Card(13, "S"), Card(14, "D"), Card(3, "D"), Card(13, "C")])
-# act ONLY CALL THE FUNCTION TO BE TESTED
-        actual = cardValues(hand)
-# assert NO LOGIC
-        self.assertEqual([13, 14, 3, 13], actual)
-
-    ###########
-    def test_cardCounter0(self):
-        hand = Hand([Card(13, "S"), Card(14, "D"), Card(3, "D"), Card(13, "C")])
-        self.assertEqual({13: 2, 14: 1, 3: 1}, cardCounter(hand))
-
-    def test_cardCounter1(self):
-        hand = Hand([Card(3, "S"), Card(3, "D"), Card(3, "C"), Card(14, "S")])
-        self.assertEqual({3: 3, 14 : 1}, cardCounter(hand))
-
-    def test_cardCounter2(self):
-        hand = Hand([Card(5, "S"), Card(5, "D"), Card(5, "C"), Card(5, "S")])
-        self.assertEqual({5: 4}, cardCounter(hand))
-    ###########
-    def test_matchReturn0(self):
-        hand = Hand([Card(13, "C"), Card(3, "S"), Card(2, "S"), Card(13, "S")])
-        actual = matchReturn(hand)
-        self.assertEqual("pair", actual)
-
-    def test_matchReturn1(self):
-        hand = Hand([Card(3, "C"), Card(3, "S"), Card(2, "S"), Card(3, "S")])
-        actual = matchReturn(hand)
-        self.assertEqual("three", actual)
-
-    def test_matchReturn2(self):
-        hand = Hand([Card(5, "C"), Card(5, "H"), Card(5, "D"), Card(5, "S")])
-        actual = matchReturn(hand)
-        self.assertEqual("four", actual)
-
-    def test_matchReturn3(self):
-        hand = Hand([Card(5, "C"), Card(5, "H"), Card(2, "D"), Card(2, "S"), Card(2, "D")])
-        actual = matchReturn(hand)
-        self.assertEqual("full house", actual)
-    ###########
-    def test_suitCounter1(self):
-        hand = Hand([Card(10, "C"), Card(2, "C"), Card(4, "D"), Card(5, "D")])
-        self.assertEqual({"C": 2, "D": 2}, suitCounter(hand))
-
-    def test_suitCounter2(self):
-        hand = Hand([Card(13, "C"), Card(3, "S"), Card(2, "S"), Card(13, "S")])
-        self.assertEqual({"S": 3, "C": 1}, suitCounter(hand))
-
-    ###########
-    def test_isStraight0(self):
-        hand = Hand([Card(2, "S"), Card(4, "C"), Card(3, "C"), Card(6, "D"), Card(5, "H")])
-        actual = isStraight(hand)
-        self.assertEqual("Straight 2 through 6", actual)
-
-    def test_isStraight1(self):
-        hand = Hand([Card(10, "S"), Card(14, "C"), Card(11, "C"), Card(13, "D"), Card(11, "H")])
-        actual = isStraight(hand)
-        self.assertEqual("Straight 10 through 14", actual)
-    ###########
-    def test_highCard(self):
-        hand = Hand([Card(10, "S"), Card(14, "C"), Card(9, "C"), Card(13, "D"), Card(11, "H")])
-        actual = highCard(hand)
-        self.assertEqual(14, actual)
-    ###########
-    def test_controller0(self):
-        hand = Hand([Card(10, "S"), Card(10, "C"), Card(11, "C"), Card(13, "D"), Card(5, "H")])
-        actual = controller(hand)
-        self.assertEqual("pair", actual)
-
-    def test_controller1(self):
-        hand = Hand([Card(10, "S"), Card(10, "C"), Card(10, "C"), Card(13, "D"), Card(5, "H")])
-        actual = controller(hand)
-        self.assertEqual("three", actual)
-
-    def test_controller2(self):
-        hand = Hand([Card(10, "S"), Card(10, "C"), Card(10, "C"), Card(10, "D"), Card(5, "H")])
-        actual = controller(hand)
-        self.assertEqual("four", actual)
-
-    def test_controller3(self):
-        hand = Hand([Card(10, "S"), Card(10, "S"), Card(10, "S"), Card(10, "S"), Card(5, "S")])
-        actual = controller(hand)
-        self.assertEqual("flush", actual)
-
-    def test_controller4(self):
-        hand = Hand([Card(3, "C"), Card(4, "S"), Card(5, "S"), Card(6, "S"), Card(7, "S")])
-        actual = controller(hand)
-        self.assertEqual("Straight 3 through 7", actual)
-
-    def test_controller5(self):
-        hand = Hand([Card(3, "C"), Card(4, "S"), Card(5, "S"), Card(6, "S"), Card(14, "S")])
-        actual = controller(hand)
-        self.assertEqual(14, actual)
-
+    def test_makeCard2(self):
+        card = makeCard("KD")
+        self.assertEqual("D", card.suit)
