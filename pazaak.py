c78694bd2ad63d8677e8d733bbf9e7217db9c853
FlimothyCrow <discman15@hotmail.com>
2020-01-04 17:44:46 -0700

    defined makeHand

--- pazaak.py
+++ pazaak.py
@@ -1,11 +1,14 @@
 import random
 
-def playCard() :
-    return 1
 
 def chooseCard() :
     cards = {1:1, 2:2, 3:3, 4:-4}
     return cards[(random.randint(1, 4))]
-    #
 
+def makeHand() :
+    hand = {}
+    for card in range(5) :
+        hand[card] = random.randint(1,20)
+    return hand
 
+print(makeHand())
--- pazaakTest.py
+++ pazaakTest.py
@@ -3,9 +3,6 @@ from pazaak import *
 
 
 class pazaakTest(unittest.TestCase):
-    def test_playCard(self):
-        self.assertEqual(1, playCard())
-
     #def test_chooseCard(self):
      #   self.assertEqual(1, chooseCard()) this passes without random.randint
 
