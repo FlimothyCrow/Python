e4b83903b4c230c502da79d710aaec528c4f18d3
FlimothyCrow <discman15@hotmail.com>
2020-01-05 11:53:50 -0700

    defined sumCards and test

--- pazaak.py
+++ pazaak.py
@@ -6,17 +6,23 @@ class Card :
         self.operator = operator
 
 class Hand :
-    def __init__(self, card):
-        self.card = card
+    def __init__(self, cards):
+        self.cards = cards
 
 def makeCard() :
     operators = {0:"P", 1:"N", 2:"B"}
     return Card(random.randint(1, 20), operators[random.randint(0,2)])
 
-print(makeCard().operator)
+#print(makeCard().operator)
 
 def makeHand() :
-    hand = {}
-    for card in range(6) :
-        hand[card] = random.randint(1,20)
-    return hand
+    cards = []
+    for card in range(3) :
+        cards.append(makeCard())
+    return Hand(cards)
+
+def sumCards(hand) :
+    totalValue = 0
+    for card in hand.cards :
+        totalValue += card.value
+    return totalValue
--- pazaakTest.py
+++ pazaakTest.py
@@ -6,3 +6,11 @@ class pazaakTest(unittest.TestCase):
     def test_makeCard(self):
         card = makeCard()
         self.assertEqual("f", card.operator) # this passes without the randint generators
+
+    def test_makeHand(self):
+        hand = makeHand()
+        self.assertEqual("P", hand.cards[0].operator)
+
+    def test_sumCards(self):
+        hand = Hand([Card(3, "P"), Card(2, "P")])
+        self.assertEqual(5, sumCards(hand))
