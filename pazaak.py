9d9a03b7ea0407a063a88767c585bd1f696966bb
FlimothyCrow <discman15@hotmail.com>
2020-01-06 20:08:05 -0700

    defined gameController

--- pazaak.py
+++ pazaak.py
@@ -35,13 +35,45 @@ def sumCards(hand) :
     return totalValue
 
 def aiCard(hand, total) :
-    sortedCards = sorted(hand.cards, key=lambda x: x.value, reverse=True)
-    reverseSortCards = sorted(hand.cards, key=lambda x: x.value)
-    if total < 20 :
-        for card in sortedCards :
-            if card.cardValue() + total <= 20 :
-                return card
-    else :
-        for card in reverseSortCards :
-            if card.cardValue() + total <= 20 :
-                return card
+    if total == 20 :
+       return None
+    sortedCards = sorted(hand.cards, key=lambda x: x.cardValue(), reverse=True)
+    for card in sortedCards :
+        if card.cardValue() + total <= 20 :
+            del hand.cards[0]
+            return card
+# draw, play or stay
+
+def playGame(hand, drawDeck) :
+    total = 0
+    while total < 15 :
+        total += drawDeck[0]
+        del drawDeck[0]
+    card = aiCard(hand, total)
+    if card:
+        total += card.cardValue()
+    return total
+
+def gameController(hand, drawDeck) :
+    total = 0
+    AI = 0
+    Player = 0
+    tie = 0
+    for turn in range(3) :
+        total = playGame(hand, drawDeck)
+        if total == 20 :
+            return "AI wins"
+        elif total > 20 :
+            return "wings of pastrami"
+        else :
+            return "humanity wins"
+
+# story 1 - make controller that plays 3 rounds of game and returns win count
+#           is given a longer "draw deck"
+
+# story 2 - run a manual simulation on controller with a real random draw deck and same hand and
+#           several different numbers for the 15 card limit and see which
+#           number provides the highest average totals below 20  (optionally play two ais against each other
+#           with differnt numbers passed in for the "15" amount and see which wins most)
+
+# story 3 - make aicard choose positive or negative MIND BLOWN
--- pazaakTest.py
+++ pazaakTest.py
@@ -22,7 +22,8 @@ class pazaakTest(unittest.TestCase):
 
     def test_aiCard(self):
         hand = Hand([Card(1, "P"), Card(2, "P"), Card(3, "P"), Card(5, "P")])
-        self.assertEqual(hand.cards[2], aiCard(hand, 16))
+        cards_ = hand.cards[2]
+        self.assertEqual(cards_, aiCard(hand, 16))
 
     def test_aiCard2(self):
         hand = Hand([Card(8, "P"), Card(2, "P"), Card(3, "P"), Card(5, "P")])
@@ -30,16 +31,55 @@ class pazaakTest(unittest.TestCase):
 
     def test_aiCard3(self):
         hand = Hand([Card(1, "P"), Card(2, "P"), Card(4, "P"), Card(5, "P")])
-        self.assertEqual(hand.cards[2], aiCard(hand, 16))
+        cards_ = hand.cards[2]
+        self.assertEqual(cards_, aiCard(hand, 16))
 
     def test_aiCard4(self):
         hand = Hand([Card(1, "P"), Card(2, "N"), Card(4, "P"), Card(5, "P")])
-        self.assertEqual(hand.cards[1], aiCard(hand, 21))
+        cards_ = hand.cards[1]
+        self.assertEqual(cards_, aiCard(hand, 21))
 
     def test_aiCard5(self):
         hand = Hand([Card(1, "P"), Card(4, "N"), Card(3, "N"), Card(5, "P")])
-        self.assertEqual(hand.cards[2], aiCard(hand, 23))
+        cards_ = hand.cards[2]
+        self.assertEqual(cards_, aiCard(hand, 23))
+        self.assertEqual(len(hand.cards), 3)
 
     def test_aiCard6(self):
         hand = Hand([Card(2, "P"), Card(3, "N")])
-        self.assertEqual(hand.cards[1], aiCard(hand, 21))
+        cards_ = hand.cards[1]
+        self.assertEqual(cards_, aiCard(hand, 21))
+
+    def test_aiCard7(self):
+        hand = Hand([Card(2, "P"), Card(3, "N")])
+        self.assertEqual(None, aiCard(hand, 20))
+
+    def test_playGame(self):
+        hand = Hand([Card(2, "P"), Card(3, "N")])
+        drawDeck = [10, 5, 8]
+        self.assertEqual(17, playGame(hand, drawDeck))
+
+    def test_playGame2(self):
+        hand = Hand([Card(5, "P"), Card(2, "N")])
+        drawDeck = [10, 4, 8]
+        self.assertEqual(20, playGame(hand, drawDeck))
+
+    def test_playGame3(self):
+        hand = Hand([Card(5, "P"), Card(2, "P")])
+        drawDeck = [10, 4, 8]
+        self.assertEqual(22, playGame(hand, drawDeck))
+
+    def test_gameController(self):
+        hand = Hand([Card(5, "P"), Card(2, "P")])
+        drawDeck = [10, 10, 10]
+        self.assertEqual("AI wins", gameController(hand, drawDeck))
+
+    def test_gameController2(self):
+        hand = Hand([Card(5, "P"), Card(2, "P")])
+        drawDeck = [10, 4, 10]
+        self.assertEqual("wings of pastrami", gameController(hand, drawDeck))
+
+    def test_gameController3(self):
+        hand = Hand([Card(5, "P"), Card(2, "P")])
+        drawDeck = [10, 4, 10]
+        self.assertEqual("humanity wins", gameController(hand, drawDeck))
