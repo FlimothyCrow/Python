74b1dab3a3fb96289d3416a776ab14bb4341da2e
FlimothyCrow <discman15@hotmail.com>
2020-02-25 22:02:18 -0700

    researching floating points

--- changeCalculator.js
+++ changeCalculator.js
@@ -1,4 +1,7 @@
 function changeCalculator(cost, paid) {
   totalDue = paid - cost
-  return totalDue
-}
+  return totalDue % 0.05
+  //if (totalDue % 0.05 === 0.0){
+    //return totalDue / 0.05
+  }
+//}
--- changeCalculatorTest.js
+++ changeCalculatorTest.js
@@ -1,6 +1,6 @@
 test('changeCalculator', function(assert) {
-  var cost = 1.75
+  var cost = 1
   var paid = 3
   var changeDue = changeCalculator(cost, paid) ;
-  assert.equal(changeDue, [[1, dollar], [1, quarter]]) ;
+  assert.equal(changeDue, "true") ;
 });
