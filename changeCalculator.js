dda43c505e13bb7967744b30f226e9b64c0098f5
FlimothyCrow <discman15@hotmail.com>
2020-02-27 14:04:11 -0700

    added conditionals to do while loop

--- changeCalculator.js
+++ changeCalculator.js
@@ -2,9 +2,18 @@ function changeCalculator(cost, paid) {
   changeDue = []
   totalDue = paid - cost
   do {
+    if (totalDue > 9){
+      changeDue.push(10);
+      totalDue -=10;
+    }
+    else if (totalDue > 4) {
     changeDue.push(5);
-    totalDue -= 5
-    totalDue++;
+    totalDue -= 5;
+    }
+    else {
+    changeDue.push(1);
+    totalDue -= 1;
+    }
   } while (totalDue > 4);
   return changeDue.length
 }
--- changeCalculatorTest.js
+++ changeCalculatorTest.js
@@ -1,6 +1,20 @@
-test('changeCalculator', function(assert) {
+test('changeCalculator0', function(assert) {
   var cost = 100 // one dollar
   var paid = 300 // three dollars
   var changeDue = changeCalculator(cost, paid) ;
-  assert.equal(changeDue, 40) ;
+  assert.equal(changeDue, 20) ; //dimes
+});
+
+test('changeCalculator1', function(assert) {
+  var cost = 100 // one dollar
+  var paid = 200 // three dollars
+  var changeDue = changeCalculator(cost, paid) ;
+  assert.equal(changeDue, 10) ;
+});
+
+test('changeCalculator2', function(assert) {
+  var cost = 7 // one dollar
+  var paid = 9 // three dollars
+  var changeDue = changeCalculator(cost, paid) ;
+  assert.equal(changeDue, 2) ;
 });
