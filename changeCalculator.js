8e500da7ab9e1bcfbcfc30d0d321e10d49476bcb
FlimothyCrow <discman15@hotmail.com>
2020-03-01 07:55:29 -0700

    added quarters and dollars + tests

--- changeCalculator.js
+++ changeCalculator.js
@@ -4,10 +4,20 @@ function changeCalculator(cost, paid) {
     pennies: 0,
     nickels: 0,
     dimes: 0,
-    quarters: 0
+    quarters: 0,
+    dollars: 0
   };
   while (totalDue > 0){
-    if (totalDue > 9){
+    if (totalDue > 99) {
+    changeObject.dollars += 1
+    console.log(changeObject)
+    totalDue -= 100;
+    }
+    else if (totalDue > 24) {
+    changeObject.quarters += 1
+    totalDue -= 25;
+    }
+    else if (totalDue > 9){
       //console.log("change object", changeObject)
       changeObject.dimes += 1
       totalDue -= 10;
--- changeCalculatorTest.js
+++ changeCalculatorTest.js
@@ -1,8 +1,8 @@
 test('changeCalculator0', function(assert) {
-  var cost = 100 // one dollar
+  var cost = 280 // one dollar
   var paid = 300 // three dollars
   var changeDue = changeCalculator(cost, paid) ;
-  assert.equal(changeDue.dimes, 20) ; //dimes
+  assert.equal(changeDue.dimes, 2) ; //dimes
 });
 
 test('changeCalculator1', function(assert) {
@@ -17,4 +17,18 @@ test('changeCalculator2', function(assert) {
   var paid = 9 // three dollars
   var changeDue = changeCalculator(cost, paid) ;
   assert.equal(changeDue.pennies, 2) ;
+});
+
+test('changeCalculator3', function(assert) {
+  var cost = 100 // one dollar
+  var paid = 180 // three dollars
+  var changeDue = changeCalculator(cost, paid) ;
+  assert.equal(changeDue.quarters, 3) ;
+});
+
+test('changeCalculator3', function(assert) {
+  var cost = 100 // one dollar
+  var paid = 500 // three dollars
+  var changeDue = changeCalculator(cost, paid) ;
+  assert.equal(changeDue.dollars, 4) ;
 });
