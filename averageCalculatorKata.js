1cdd67627e36e12a98652d806a435fccc3439cb9
FlimothyCrow <discman15@hotmail.com>
2020-03-03 15:40:25 -0700

    defined modeCalculator and test

--- averageCalculatorKata.js
+++ averageCalculatorKata.js
@@ -5,3 +5,22 @@ function averageCalculator(integers) {
   }
   return sumOfInts / integers.length
 }
+
+function meanCalculator(listOfInts){
+  if (listOfInts.length % 2 === 0){
+    return "pass"
+  }
+  else {
+    return listOfInts[(listOfInts.length / 2) - 0.5]
+  }
+}
+
+function modeCalculator(listOfInts){
+  modeList = []
+  for (i = 0; i < listOfInts.length; i++){
+    if (listOfInts[i] === listOfInts[i+1]){
+      modeList.push(listOfInts[i])
+    }
+  }
+  return modeList
+}
--- averageCalculatorTests.js
+++ averageCalculatorTests.js
@@ -7,3 +7,25 @@ test('averageCalculator', function(assert) {
   var listOfInts = averageCalculator([1, 10, 100, 5, 39])
   assert.equal(listOfInts, 31) ;
 });
+//---------------------------------------
+
+test('meanCalculator', function(assert){
+  var listOfInts = meanCalculator([1, 2, 3, 4, 5, 6])
+  assert.equal(listOfInts, "pass") ;
+});
+
+test('meanCalculator', function(assert){
+  var listOfInts = meanCalculator([1, 2, 3, 4, 5])
+  assert.equal(listOfInts, 3) ;
+});
+
+test('meanCalculator', function(assert){
+  var listOfInts = meanCalculator([1, 2, 3, 4, 5, 6, 7, 8, 9])
+  assert.equal(listOfInts, 5) ;
+});
+//---------------------------------------
+
+test('modeCalculator', function(assert){
+  var listOfInts = modeCalculator([1, 1, 2, 9, 39, 238]) ;
+  assert.equal(listOfInts, 1)
+})
