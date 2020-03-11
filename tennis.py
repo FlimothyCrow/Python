d0423d077cbeebe06bbdb0d3077a61dc99ebc435
FlimothyCrow <discman15@hotmail.com>
2020-03-11 12:06:45 -0600

    anagrams completed with passing tests

--- tennis.py
+++ tennis.py
@@ -52,3 +52,11 @@ class UserMainCode(object):
             nestedList.append(innerList)
         return nestedList
         pass
+
+def anagrams(list0, list1) :
+    orderedList0 = sorted(list0, key = str)
+    orderedList1 = sorted(list1, key = str)
+    if orderedList0 == orderedList1 :
+        return True
+    else:
+        return False
