1788aa9b702159c65c4b5b4157bd6dc080409767
FlimothyCrow <discman15@hotmail.com>
2021-05-24 15:45:14 -0600

    updated counter logic in renameUtility()

--- renameUtility.py
+++ renameUtility.py
@@ -9,12 +9,17 @@ def renameUtility(path):
     for index, file in enumerate(files):
         if os.path.isdir(os.path.join(path, ''.join(file))):
             renameUtility(os.path.join(path, ''.join(file)))
+            name = list(file)
+            newName = [' ' if i in [".", "[", "]", "{", "}", "(", ")", "+", "-"] else i for i in name[:-4]]
+            newName = ''.join(newName + name[-4:])
+            counter += 1 if file != newName else counter
+            os.rename(os.path.join(path, file), os.path.join(path, ''.join(removeSpaces(newName))))
         else :
             name = list(file)
             newName = [' ' if i in [".", "[", "]", "{", "}", "(", ")", "+", "-"] else i for i in name[:-4]]
             newName = ''.join(newName + name[-4:])
+            counter += 1 if file != newName else counter
             os.rename(os.path.join(path, file), os.path.join(path, ''.join(removeSpaces(newName))))
-            counter += 1
     print("total files renamed: %s" % counter)
 
 renameUtility("C:\\Users\\Timothy\\Desktop\\target folder\\")
@@ -22,4 +27,3 @@ renameUtility("C:\\Users\\Timothy\\Desktop\\target folder\\")
 # renameUtility("D:\\Flim's Documents\\gegl-0.0\\plug-ins\\archives\BitTorrent downloads (complete)\\to be checked\\ww")
 
 # build path parser to avoid having to add \\
-# add removeSpaces() to (if os.path.isdir)
